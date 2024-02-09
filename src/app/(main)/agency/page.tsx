0033d32dc2438e4aa3ba43cbfcd4c8007ac9cdda
Mansha Qarib <mansha.qarib777@gmail.com>
2024-02-09 17:09:02 +0500

    fixes

---
+++ src/app/(main)/agency/page.tsx
@@ -0,0 +1,19 @@
+import { getAuthUserDetails, verifyAndAcceptInvitation } from '@/lib/queries'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+const Page = async () => {
+  const authUser = await currentUser()
+  if(!authUser) return redirect('/sign-in')
+
+  const agencyId = await verifyAndAcceptInvitation()
+  console.log(agencyId)
+  //get users details  
+  const user = await getAuthUserDetails()
+  return (
+    <div>Agency Page</div>
+  )
+}
+
+export default Page
